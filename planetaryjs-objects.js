d89caaae3236b75c36dd5db68bf3f9a023d9717d
Sheaffy <sheaffy.ashley@gmail.com>
2017-10-17 23:49:01 +0100

    MainUpdate

--- planetaryjs-objects.js
+++ planetaryjs-objects.js
@@ -4,9 +4,6 @@
 
     var addObject = function(lng, lat, options) {
       options = options || {};
-      options.color = options.color || config.color || 'white';
-      options.angle = options.angle || config.angle || 5;
-      options.ttl   = options.ttl   || config.ttl   || 2000;
       
       options.speed = options.speed || config.speed || 10;
 
@@ -24,29 +21,23 @@
     var drawobjects = function(planet, context, now) {
       var newobjects = [];
       for (var i = 0; i < objects.length; i++) {
-        var ping = objects[i];
-        var alive = now - ping.time;
-        if (alive < ping.options.ttl) {
-          newobjects.push(ping);
-          drawobject(planet, context, now, alive, ping);
-        }
+        var object = objects[i];
+        
+        newobjects.push(object);
+        drawobject(planet, context, now, object);
+        
       }
       objects = newobjects;
     };
 
-    var drawobject = function(planet, context, now, alive, ping) {
+    var drawobject = function(planet, context, now, object) {
       
-      var coords = planet.projection([(ping.lng), ping.lat])  
+      var coords = planet.projection([(object.lng), object.lat])  
       var img = new Image()
       img.src = "/char.png";
-      img.append(planet.path)
+      
 
-      var geoangle = d3.geo.distance(
-              [ping.lng, ping.lat],
-              [
-                  -planet.projection.rotate()[0],
-                  -planet.projection.rotate()[1]
-              ]);
+      var geoangle = d3.geo.distance([object.lng, object.lat],[-planet.projection.rotate()[0], -planet.projection.rotate()[1]]);
 
       if (geoangle > 1.57079632679490)
       {
